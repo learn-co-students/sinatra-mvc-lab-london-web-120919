13cfe0aff8c92938f522f1307969befcf80caa38
James <jinglis12@googlemail.com>
2020-01-08 17:35:12 +0000

    Done.

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 describe 'class PigLatinizer' do
   let!(:words) { PigLatinizer.new }
-
 
   it 'can create a new instance of a class' do
     expect(PigLatinizer.new).to be_an_instance_of(PigLatinizer)
   end
 
   it 'piglatinizes an individual word' do
-    expect(words.piglatinize("pork")).to eq("orkpay")
-    expect(words.piglatinize("I")).to eq("Iway")
-    expect(words.piglatinize("hello")).to eq("ellohay")
-    expect(words.piglatinize("please")).to eq("easeplay")
-    expect(words.piglatinize("tomorrow")).to eq("omorrowtay")
-    expect(words.piglatinize("until")).to eq("untilway")
-    expect(words.piglatinize("this")).to eq("isthay")
-    expect(words.piglatinize("Enumeration")).to eq("Enumerationway")
-    expect(words.piglatinize("spray")).to eq("ayspray")
-    expect(words.piglatinize("prays")).to eq("ayspray")
+    expect(words.translate('pork')).to eq('orkpay')
+    expect(words.translate('I')).to eq('Iway')
+    expect(words.translate('hello')).to eq('ellohay')
+    expect(words.translate('please')).to eq('easeplay')
+    expect(words.translate('tomorrow')).to eq('omorrowtay')
+    expect(words.translate('until')).to eq('untilway')
+    expect(words.translate('this')).to eq('isthay')
+    expect(words.translate('Enumeration')).to eq('Enumerationway')
+    expect(words.translate('spray')).to eq('ayspray')
+    expect(words.translate('prays')).to eq('ayspray')
   end
 
   it 'has a method splits the sentence to piglatinize each word' do
-    expect(words.piglatinize("i love programming")).to eq("iway ovelay ogrammingpray")
+    expect(words.translate('i love programming')).to eq('iway ovelay ogrammingpray')
   end
-
 end
